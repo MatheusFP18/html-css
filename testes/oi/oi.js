7702eb58369f1458d1090dbddaa37a276f2ac6f8
MatheusFP18 <matheusfelippe15r@gmail.com>
2022-11-11 00:05:55 -0300

    atualização

---
+++ testes/oi/oi.js
@@ -0,0 +1,8 @@
+penColour('#ff0000');
+circle(50, 70, 10);
+penColour('#3333ff');
+rect(50, 40, 20, 40);
+penColour('#ff0000');
+line(40, 50, 20, 70, 5);
+penColour('#000000');
+line(80, time(), 60, 50, 5);
